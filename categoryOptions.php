2aadc6538aa3e1d25de7e40928139b3b27f3a528
adharshini <adharshinichandrasekar@gmail.com>
2024-04-22 20:15:24 +0530

    first commit

---
+++ categoryOptions.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<option>- All Job Postings -</option>
+<option>Accounting Jobs</option>
+<option>Interior Design Jobs</option>
+
+<option>Engineering Jobs</option>
+
+<option>Security Jobs</option>
+
+
+
+<option>IT Jobs</option>
+<option>Testing Jobs</option>
+<option>System Programming Jobs</option>
+
+
+<option>Sales Jobs</option>
+<option>Secretary Jobs</option>
+
+<option>Graphic Designer Jobs</option>
+
+<option>Analytics Jobs</option>
---
+++ industryOptions.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+<option>Automobile , Auto Anciliary , Auto Components</option>
+
+<option>Export , Import</option>
+<option>Facility Management</option>
+<option>Fertilizers , Pesticides</option>
+
+<option>Fresher , Trainee , Entry Level</option>
+
+<option>Government , Defence</option>
+
+<option>Industrial Products , Heavy Machinery</option>
+<option>Insurance</option>
+<option>Internet , Ecommerce</option>
+<option>Iron and Steel</option>
+<option>IT-Hardware & Networking</option>
+<option>IT-Software , Software Services</option>
+<option>KPO , Research , Analytics</option>
+<option>Leather</option>
+
+
+
+
